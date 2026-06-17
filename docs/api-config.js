773bd5a0cfbf587acf37cd132bd0907b842af0b1
Anirudha Talmale <anirudhatalmale22@gmail.com>
2026-06-17 13:58:18 +0000

    Update tunnel URL

--- docs/api-config.js
+++ docs/api-config.js
@@ -1,2 +1,2 @@
 // Backend API URL - update this when the tunnel URL changes
-window.API_BASE = 'https://unlimited-thumbs-controlling-grant.trycloudflare.com';
+window.API_BASE = 'https://angle-satisfaction-common-watt.trycloudflare.com';
